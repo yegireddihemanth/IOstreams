aefc63b817b7c2dc9efef271c0226b351c9d89da
hemanth <hemanthyegireddyad@gmail.com>
2022-05-17 10:41:08 +0530

    done

---
+++ IOstreams/src/ai/jobiak/io/ConsoleToFile.java
@@ -0,0 +1,11 @@
+package ai.jobiak.io;
+import java.io.*;
+public class ConsoleToFile {
+	
+public static void main(String[] args) throws IOException {
+	
+	FileWriter writer = new FileWriter("mydata.txt");
+	DataInputStream data = DataInputStream(System.in);
+}
+	
+}
---
+++ IOstreams/src/ai/jobiak/io/FileToConsole.java
@@ -0,0 +1,15 @@
+package ai.jobiak.io;
+import java.io.*;
+
+import java.io.FileInputStream;
+
+public class FileToConsole {
+
+	public static void main(String[] args)throws IOException {
+		
+		//FileInputStream fis = new FileInputStream("D:\CORE\IOstreams\src\ai\jobiak\io\FileToConsole.java");
+		//int data = fis.read();
+
+	}
+
+}
